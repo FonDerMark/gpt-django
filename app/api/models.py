da9849c03f6acc34e74abcfa8eb39b8cf63d4349
Dmitry <68408042+FonDerMark@users.noreply.github.com>
2023-05-09 09:29:12 +0500

    0.0.4

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Messages(models.Model):
+    datetime = models.DateTimeField()
+    text = models.CharField(max_length=5000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'message'
+        verbose_name_plural = 'messages'
+        ordering = ['datetime']
