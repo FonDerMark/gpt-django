cf343e2d62dfac6437c8e5bff9b4c812676d0f7a
Xm0rph <publicnox@gmail.com>
2023-05-10 23:06:39 +0500

    0.2.5

--- app/api/models.py
+++ app/api/models.py
@@ -7,6 +7,9 @@ class Messages(models.Model):
     text = models.CharField(max_length=5000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return self.user.username
+
     class Meta:
         verbose_name = 'message'
         verbose_name_plural = 'messages'
