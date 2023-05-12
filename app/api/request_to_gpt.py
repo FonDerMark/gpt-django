68ac1ffeea726956e3afe28c98adb20818e88343
Xm0rph <publicnox@gmail.com>
2023-05-12 14:39:00 +0500

    0.3.7

--- app/api/request_to_gpt.py
+++ app/api/request_to_gpt.py
@@ -8,9 +8,9 @@ default_gpt_mode = 'theb'
 
 def request_to_gpt(question, mode=default_gpt_mode):
     response = {
-        'you': lambda: gpt4free.Completion.create(Provider.You, prompt=question),
+        'you': lambda: gpt4free.Completion.create(Provider.You, prompt=question).encode().decode('unicode_escape'),
         'poe': lambda: gpt4free.Completion.create(Provider.Poe, prompt=question, token=quora.Account.create(logging=False), model='ChatGPT'),
         'forefront': lambda: gpt4free.Completion.create(Provider.ForeFront, prompt=question, model='gpt-4', token=quora.Account.create(logging=False)),
         'theb': lambda: gpt4free.Completion.create(Provider.Theb, prompt=question),
     }.get(mode)()
-    return response.encode().decode('utf-8')
+    return response
